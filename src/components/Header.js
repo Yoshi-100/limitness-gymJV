15aee6298adf7085d3d52777458ca1378d7e39d8
Yoshi-100 <taguchi-y@jpn.marubeni.com>
2025-03-04 14:29:45 +0900

    Create Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <div className="container">
+        <h1>リミットネス Gym</h1>
+        <p>～あなたの限界を超える場所～</p>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
